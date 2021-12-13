bef0202f270d10b96f3577bd06418a22e4cbf3e5
Yongming Fan <fanym0919@gmail.com>
2021-12-13 00:01:00 -0500

    more test

--- src/groth16.cpp
+++ src/groth16.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 
 #include "libff/algebra/fields/field_utils.hpp"
-#include "libsnark/zk_proof_systems/ppzksnark/r1cs_ppzksnark/r1cs_ppzksnark.hpp"
-#include "libsnark/common/default_types/r1cs_ppzksnark_pp.hpp"
+#include "libsnark/zk_proof_systems/ppzksnark/r1cs_gg_ppzksnark/r1cs_gg_ppzksnark.hpp"
+#include "libsnark/common/default_types/r1cs_gg_ppzksnark_pp.hpp"
 #include "libsnark/gadgetlib1/pb_variable.hpp"
 
 using namespace libsnark;
@@ -11,11 +11,11 @@ using namespace std;
 
 int main()
 {
-  typedef libff::Fr<default_r1cs_ppzksnark_pp> FieldT;
+  typedef libff::Fr<default_r1cs_gg_ppzksnark_pp> FieldT;
 
   // Initialize the curve parameters
 
-  default_r1cs_ppzksnark_pp::init_public_params();
+  default_r1cs_gg_ppzksnark_pp::init_public_params();
   
   // Create protoboard
 
@@ -67,18 +67,18 @@ int main()
 
   const r1cs_constraint_system<FieldT> constraint_system = pb.get_constraint_system();
 
-  const r1cs_ppzksnark_keypair<default_r1cs_ppzksnark_pp> keypair = r1cs_ppzksnark_generator<default_r1cs_ppzksnark_pp>(constraint_system);
+  const r1cs_gg_ppzksnark_keypair<default_r1cs_gg_ppzksnark_pp> keypair = r1cs_gg_ppzksnark_generator<default_r1cs_gg_ppzksnark_pp>(constraint_system);
 
-  const r1cs_ppzksnark_proof<default_r1cs_ppzksnark_pp> proof = r1cs_ppzksnark_prover<default_r1cs_ppzksnark_pp>(keypair.pk, pb.primary_input(), pb.auxiliary_input());
+  const r1cs_gg_ppzksnark_proof<default_r1cs_gg_ppzksnark_pp> proof = r1cs_gg_ppzksnark_prover<default_r1cs_gg_ppzksnark_pp>(keypair.pk, pb.primary_input(), pb.auxiliary_input());
 
-  bool verified = r1cs_ppzksnark_verifier_strong_IC<default_r1cs_ppzksnark_pp>(keypair.vk, pb.primary_input(), proof);
+  bool verified = r1cs_gg_ppzksnark_verifier_strong_IC<default_r1cs_gg_ppzksnark_pp>(keypair.vk, pb.primary_input(), proof);
 
   cout << "Number of R1CS constraints: " << constraint_system.num_constraints() << endl;
   cout << "Primary (public) input: " << pb.primary_input() << endl;
   cout << "Auxiliary (private) input: " << pb.auxiliary_input() << endl;
   cout << "Verification status: " << verified << endl;
 
-  const r1cs_ppzksnark_verification_key<default_r1cs_ppzksnark_pp> vk = keypair.vk;
+  const r1cs_gg_ppzksnark_verification_key<default_r1cs_gg_ppzksnark_pp> vk = keypair.vk;
 
   return 0;
 }
